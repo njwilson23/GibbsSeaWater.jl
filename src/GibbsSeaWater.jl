4c8ec4ece0443362d204b26a514737fd095e4d4f
Nat Wilson <njwilson23@gmail.com>
2015-04-14 15:03:09 -0700

    first working GSW function

--- src/GibbsSeaWater.jl
+++ src/GibbsSeaWater.jl
@@ -1,5 +1,12 @@
 module GibbsSeaWater
 
 # package code goes here
+const _MODULEPATH = Pkg.dir("GibbsSeaWater")
+const _LIBGSWPATH = joinpath(MODULEPATH, "deps/build/libgswteos-10")
+
+function sigma0(sa, ct)
+    return ccall((:gsw_sigma0, _LIBGSWPATH), Float64, (Float64, Float64),
+                 sa, ct)
+end
 
 end # module
