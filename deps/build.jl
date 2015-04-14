bdf6f21044005fc41c7947a91892968c75451e9e
Nat Wilson <njwilson23@gmail.com>
2015-04-14 15:02:46 -0700

    add simple build script (probably only works on linux right now)

---
+++ deps/build.jl
@@ -0,0 +1,37 @@
+
+function build()
+    run(`make clean`)
+    run(`make library`)
+end
+cd(build, "src/gsw_c_v3.03/")
+
+if ~isdir("build")
+    mkdir("build")
+end
+run(`cp src/gsw_c_v3.03/libgswteos-10.so build/libgswteos-10.so`)
+
+# using BinDeps
+# 
+# @BinDeps.setup
+# 
+# gsw = library_dependency("gsw")
+# provides(Sources,
+#          URI("http://www.teos-10.org/software/gsw_c_v3.03.zip"),
+#          gsw,
+#          unpacked_dir="gsw")
+# 
+# @build_steps begin
+#     GetSources(gsw)
+#     CreateDirectory("build")
+#     FileUnpacker("gsw_c_v3.03.zip", "build")
+#     @build_steps begin
+#         ChangeDirectory("build")
+#         MakeTargets("gsw_c_v3.03")
+#         FileRule(joinpath("build", "gsw-3.03.so"), @build_steps begin
+#             `cp gsw_c_v3.03/gsw-3.03.so .`
+#         end)
+#     end
+# end
+# 
+# @BinDeps.install
+
